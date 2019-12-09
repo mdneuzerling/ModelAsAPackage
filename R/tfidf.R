e3f997b59450db404795794dd38310dd0733cfe6
Murray David Neuzerling <murray@neuzerling.com>
2019-12-09 16:05:36 +1100

    Added R files for data

---
+++ R/tfidf.R
@@ -0,0 +1,6 @@
+#' Term frequency inverse document frequency object created in model training
+#'
+#' This object from the text2vec package is used to map novel text to a
+#' document term matrix with the same weighting as that used in training
+#'
+"tfidf"
---
+++ R/vectoriser.R
@@ -0,0 +1,7 @@
+#' Vectoriser created in model training
+#'
+#' This object from the text2vec package is used to map novel text to a
+#' document term matrix with the same terms as the document term matrix used
+#' in training
+#'
+"vectoriser"
