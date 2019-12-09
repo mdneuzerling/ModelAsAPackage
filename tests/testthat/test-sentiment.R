755f23d349e9c987c85c14c4f07b8174a24f15d5
Murray David Neuzerling <murray@neuzerling.com>
2019-12-09 16:13:11 +1100

    Fixing test-sentiment

--- tests/testthat/test-sentiment.R
+++ tests/testthat/test-sentiment.R
@@ -1,7 +1,7 @@
 context("sentiment")
 
 test_that("'bad' is bad sentiment", {
-  expect_equal(sentiment("bad"), "good")
+  expect_equal(sentiment("bad"), "bad")
 })
 
 test_that("'love' is good sentiment", {
